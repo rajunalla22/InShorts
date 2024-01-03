d1d6bfce4d0c831ce4780d2106d2b46240f56fa7
rajunalla22 <nallaraju1805@gmail.com>
2024-01-03 23:06:55 +0530

    shortDescriptionNews

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import News from './components/news';
+
+function App() {
+  return (
+    <div className="App">
+      <News />
+    </div>
+  );
+}
+
+export default App;
